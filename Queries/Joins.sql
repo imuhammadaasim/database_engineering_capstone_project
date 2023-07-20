6a822896fbe14fbf6fc72e307a206a060ada6692
Muhammad Asim <72549696+imuhammadaasim@users.noreply.github.com>
2023-07-20 14:40:51 +0500

    Delete Queries directory

--- Queries/Joins.sql
+++
@@ -1,9 +0,0 @@
-select Customers.CustomerID, Customers.CustomerName, 
-Orders.OrderID, Orders.TotalCost, Menu.MenuName,
-MenuItems.CourseName, MenuItems.StarterName
-from Orders 
-inner join Customers on Orders.CustomerID = Customers.CustomerID
-inner join Menu on Orders.MenuID = Menu.MenuID
-inner join MenuItems on Menu.MenuID = MenuItems.MenuID
-where TotalCost > 50
-order by TotalCost asc;
--- Queries/SubQueries.sql
+++
@@ -1,1 +0,0 @@
-select MenuName from Menu where MenuID = any (select MenuID from Orders where Quantity > 2);
